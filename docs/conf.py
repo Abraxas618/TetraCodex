d5c2ed8a1256b2f6375d4775031d7ed5df8cf8e8
Michael Tass MacDonald <202492963+Abraxas618@users.noreply.github.com>
2025-04-15 17:42:29 -0600

    Update conf.py
    Signed-off-by: Michael Tass MacDonald <202492963+Abraxas618@users.noreply.github.com>

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,34 @@
+# -- Project Information -----------------------------------------------------
 
+project = 'TetraCodex'
+copyright = '2025, Michael Tass MacDonald'
+author = 'Michael Tass MacDonald (Abraxas618)'
+release = 'v1.0'
+
+# -- General Configuration ---------------------------------------------------
+
+extensions = [
+    'myst_parser',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+# -- MyST Configuration ------------------------------------------------------
+
+myst_enable_extensions = [
+    "deflist",
+    "fieldlist",
+    "html_admonition",
+    "html_image",
+]
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
